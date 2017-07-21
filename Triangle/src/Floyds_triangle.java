d323ab5439073268b0dcfe43df5dc189a1a62332
myeongsu <myeongsu@likelion.org>
2017-07-21 10:16:25 +0900

    17-07/21 Java Triangle Update

--- Triangle/src/Floyds_triangle.java
+++ Triangle/src/Floyds_triangle.java
@@ -8,39 +8,28 @@ public class Floyds_triangle {
         // 코드를 입력하세요.
         int a = 1;
         int temp = 0;
-        int test;
-        for (int x = 1; x <= height; x++) {
+
+        for (int x = 1; x <= height; x++) { //입력받은 최대값을 구하는 반복문
             temp = temp + x;
         }
-
         String stemp = Integer.toString(temp);
-        //stemp.length() 가 최대값의 길이
+
+
         String sa;
-        for (int i = 1; i <= height; i++) {
 
-            for (int j = 0; j < i; j++) {
-
-
+        for (int i = 1; i <= height; i++) { //행을 바꾸는 반복
+            for (int j = 0; j < i; j++) {   // 열을 바꾸는 반복
                     sa = Integer.toString(a);
-                    test = sa.length();
 
-
+                for (int x = sa.length(); x <stemp.length(); x++ ) { //자기의 길이가 최대값보다 작다면 공백 출력반복
+                    System.out.print(" ");
+                }
                        //System.out.print("@");
-                        System.out.print(test +" ");
-
+                        System.out.print(a +" ");
                     a++;
-
-
-
             }
-
             System.out.println();
         }
-
-
-
-        System.out.println(stemp.length());
-
     }
 
 
